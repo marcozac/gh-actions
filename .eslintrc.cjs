8ae18430dfa324bf3772110892d212864e6d6979
Marco <marco@marcozac.com>
2022-05-03 22:26:22 +0200

    chore: set `yarn` as èackage manager + switch to esm

---
+++ .eslintrc.cjs
@@ -0,0 +1,25 @@
+// @ts-check
+
+/** @type {import('@marcozac/eslint-config/eslint-config').Config} */
+const config = {
+    root: true,
+    extends: ['@marcozac/eslint-config'],
+    rules: {
+        'no-console': 'off',
+        '@typescript-eslint/no-use-before-define': ['error', { functions: false }],
+    },
+
+    overrides: [
+        {
+            files: ['rollup.config.js'],
+            parserOptions: {
+                sourceType: 'module',
+            },
+            rules: {
+                'import/no-extraneous-dependencies': 'off',
+                'node/no-unpublished-import': 'off',
+            },
+        },
+    ],
+};
+module.exports = config;
